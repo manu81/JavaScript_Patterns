82d96c141bd819a842c6d5d63c28d583c3c35536
manuel <manuel.diaz.mesa81@gmail.com>
2018-12-23 21:49:25 -0500

    Object module done

---
+++ Javascript_Patterns/Javascript_Patterns/Scripts/app/task.js
@@ -0,0 +1,28 @@
+﻿
+var task = {};
+
+task.title = 'My task';
+task.description = "Description";
+
+Object.defineProperty(task, 'toString', {
+    value: function () {
+        return this.title + ' ' + this.description;
+    },
+    writable: false,
+    enumerable: true,
+    configurable:true
+})
+
+
+var urgentTask = Object.create(task);
+
+Object.defineProperty(urgentTask, 'toString', {
+    value: function () {
+        return this.title + ' ' + 'is urgent';
+    },
+    writable: false,
+    enumerable: true,
+    configurable: true
+})
+
+console.log(urgentTask.toString());
